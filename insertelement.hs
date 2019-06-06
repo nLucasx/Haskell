f31a51c14f3f5255bde93b73dcfd16284fd6f6a4
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-06-06 10:24:56 -0300

    Insert an element into a sorted list

--- insertelement.hs
+++ insertelement.hs
@@ -1,5 +1,4 @@
 insert :: [Int] -> Int -> [Int]
 insert [] element = [element]
-insert (x:xs) element | (element >= x) = x : haskell xs element
-insert (x:xs) element | (element <= x) = element: x : xs
---
+insert (x:xs) element | (element >= x) = x : insert xs element
+                      | otherwise = element : x : xs
