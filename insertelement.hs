62871959f4c8a15fd28987cf1cdb817c8c51b3fb
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-06-06 10:19:53 -0300

    Insert an element into a list

---
+++ insertelement.hs
@@ -0,0 +1,4 @@
+insert :: [Int] -> Int -> [Int]
+insert [] element = [element]
+insert (x:xs) element | (element >= x) = x : haskell xs element
+insert (x:xs) element | (element <= x) = element: x : xs
